c92899d8c86e3e2573976341e8476eb83cac4fd2
indesign student <indesign@STUD.COLMAN.AC.IL>
2019-02-27 19:20:10 +0200

    message

--- Project/wifi2/wifi2.ino
+++ Project/wifi2/wifi2.ino
@@ -1,26 +1,32 @@
 #include "UbidotsMicroESP8266.h"
 
+// Ubidots
 #define TOKEN  "BBFF-EmcoWeUjJABH3U09MPNmVCF69EESB4"
+Ubidots client(TOKEN);
+
+// Wifi credentials
 #define WIFISSID "Lior"
 #define PASSWORD "lior12341234"
 
+// Global parameters and objects
 #define MAX_OBJECTS 10
-
-Ubidots client(TOKEN);
-
 const char* delim = ";";
 char objectKey[MAX_OBJECTS][20];
 int objectValue[MAX_OBJECTS];
 char objectContext[MAX_OBJECTS][40];
 
 void setup() {
+  // Initialize wifi component and connect to wifi endpoint
   Serial.begin(9600);
   delay(10);
-  client.setDebug(true);
   client.wifiConnection(WIFISSID, PASSWORD);
 }
 
 void loop() {
+  // When receiving data from Arduino board,
+  // split the data into data structurs that fit to Ubidots client
+  // and send it to Ubidots
+  
   if (Serial.available()) {
     String dataString = Serial.readString();
     char dataArr[dataString.length()];
